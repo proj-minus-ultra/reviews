6cc2dcec9ecfa9c650b3f784fe80ac4f72448f03
Alex Benko <benkoalex521@gmail.com>
2020-05-20 10:59:28 -0700

    Successfully adds to database

--- server/controller.js
+++ server/controller.js
@@ -11,6 +11,16 @@ const controller = {
         res.set('Cache-Control', 'max-age=15536000').status(200).send(results);
       }
     })
+  },
+  post: (req, res) =>{
+    database.postReview(req, (err, result) =>{
+      if(err) {
+        console.log('Error Adding Review:', err);
+        res.status(404).send(err);
+      } else {
+        res.status(201).send('Added Successfully');
+      }
+    })
   }
 
 }
