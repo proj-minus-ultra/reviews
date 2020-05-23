491424ca15851da516d53cfd7b89a777349b2ec6
Alex Benko <benkoalex521@gmail.com>
2020-05-23 09:43:30 -0700

    Modularized this so it can be used anywhere in my project with ease

--- database/seed.js
+++ database/seed.js
@@ -2,47 +2,42 @@ const Mongo = require('mongodb').MongoClient;
 const url = `mongodb://localhost:27017/reviews`;
 const faker = require('faker');
 const fs = require('fs');
-const csvWriter = require('csv-write-stream');
 
-const writer = csvWriter();
-const createReview = require('../createReviews.js').createReviews;
-
-//negative one so first set of reviews has id 0
+const createReview = require('./createReviews.js').createReviews;
 let id = -1;
+module.exports = {
 
-let csv = (writer, cb) =>{
-  writer.pipe(fs.createWriteStream('sdc.csv'));
-  console.log('Generating CSV...');
-  let i = 10000000;
-   let write=()=> {
-    let ok = true;
-    do {
-      i -= 1;
-      id += 1;
-      console.log(i);
-      let review = createReview(id);
-      review.reviews.map((rev)=>{
-        if (i === 0) {
-          writer.write(rev,cb);
-        } else {
-          // see if we should continue, or wait
-          ok = writer.write(rev);
+  csv (writer, cb){
+    writer.pipe(fs.createWriteStream('sdc.csv'));
+    console.log('Generating CSV...');
+    let i = 100;
+    let write=()=> {
+      let ok = true;
+      do {
+        i -= 1;
+        id += 1;
+        console.log(i);
+        let review = createReview(id);
+        review.reviews.map((rev)=>{
+          if (i === 0) {
+            writer.write(rev,cb);
+          } else {
+            // see if we should continue, or wait
+            ok = writer.write(rev);
+          }
+        })
+      } while (i > 0 && ok);
+        if (i > 0) {
+          // write some more once it drains
+          writer.once('drain', write);
         }
-      })
-    } while (i > 0 && ok);
-      if (i > 0) {
-        // write some more once it drains
-        writer.once('drain', write);
       }
+        write();
     }
-      write();
-  };
+}
 
 
-  csv(writer, ()=>{
-    writer.end();
-    console.log(`Successfully Generated CSV ! Time to Insert into Database`);
-  });
+
 
 
 
