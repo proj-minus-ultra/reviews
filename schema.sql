e1a87d83c6e5c93a92010f636d705270d22ccbed
Alex Benko <benkoalex521@gmail.com>
2020-05-22 17:11:10 -0700

    Schema for psql database

---
+++ schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS sdc;
+
+CREATE DATABASE sdc;
+
+CREATE TABLE reviews(
+  id INT NOT NULL primary key,
+  rating INT NOT NULL,
+  title VARCHAR(200) NOT NULL,
+  review VARCHAR(1000) NOT NULL,
+  recommendation BOOLEAN NOT NULL,
+  nickname VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  age VARCHAR(15) NOT NULL,
+  bodyType VARCHAR(15) NOT NULL,
+  locat VARCHAR(50) NOT NULL,
+  wearTo VARCHAR(50) NOT NULL,
+  likes VARCHAR(50) NOT NULL,
+  dislikes VARCHAR(50) NOT NULL
+);
