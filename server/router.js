50c91a831b022fa72bb753d22c738a9f33574787
Alex Benko <benkoalex521@gmail.com>
2020-05-26 14:57:45 -0700

    fixed routes

--- server/router.js
+++ server/router.js
@@ -2,11 +2,7 @@ const router = require('express').Router();
 const controller = require('./controller.js');
 
 router.route('/reviews')
-  .get(controller.getAll)
   .delete(controller.delete)
-
-
-router.route('/reviews/rating/:id')
 
 
 router.route('/reviewspost')
